ec4a60105b3cc18626e76f428275b44cdb6ddaa3
EDONADEI <emrick.donadei@hotmail.fr>
2018-04-17 10:43:29 +0200

    Ajout de quelques infos dans les deux classes
    Manque constructeur / destructeur

--- etat.h
+++ etat.h
@@ -6,8 +6,14 @@ using namespace std;
 class Etat
 {
 public:
+    Etat();
+    ~Etat();
 private:
 protected:
+    bool poubelle;
+    bool entree;
+    bool sortie;
+    Transition * Transitions;
 };
 
 #endif // ETAT_H_INCLUDED
--- transition.h
+++ transition.h
@@ -1,13 +1,22 @@
 #ifndef TRANSITION_H_INCLUDED
 #define TRANSITION_H_INCLUDED
+#include "Etat.h"
+
 
 using namespace std;
 
 class Transition
 {
+
 public:
+    Transition();
+    ~Transition();
 private:
 protected:
+    Etat * entrée;
+    Etat * sortie;
+    string lettre;
+
 };
 
 #endif // TRANSITION_H_INCLUDED
